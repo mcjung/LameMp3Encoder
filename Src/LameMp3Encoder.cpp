eae9c28cec934cf1ac7ed098be5d5c0fb3291590
Minchul Jung <minchul.jung@outlook.com>
2018-12-22 10:59:25 +0900

    add some codes for 1 and 2 channel

--- Src/LameMp3Encoder.cpp
+++ Src/LameMp3Encoder.cpp
@@ -18,9 +18,11 @@ bool LameMp3Encoder::encode_wav(std::string &input)
 	const size_t PCM_SIZE = 8192;
 	const size_t MP3_SIZE = 8192;
 	const size_t LAME_GOOD = 5;
-	int16_t *pcm_buffer = new int16_t[PCM_SIZE * parser.get_channels()];
+	const size_t NUM_CHANNELS = parser.get_channels();
+	const size_t BITS_PER_SAMPLE = parser.get_bits_per_sample();
+	int16_t *pcm_buffer = new int16_t[PCM_SIZE * NUM_CHANNELS];
 	unsigned char *mp3_buffer = new unsigned char [MP3_SIZE];
-	const size_t bytes_per_sample = parser.get_channels() * parser.get_bits_per_sample() / 8;
+	const size_t bytes_per_sample = NUM_CHANNELS * BITS_PER_SAMPLE / 8;
 	const string ext = { "mp3" };
 
 	string output(input);
@@ -41,6 +43,17 @@ bool LameMp3Encoder::encode_wav(std::string &input)
 	lame_set_in_samplerate(lame, IN_SAMPLERATE);
 	lame_set_VBR(lame, vbr_default);
 	lame_set_VBR_q(lame, LAME_GOOD);
+	if (NUM_CHANNELS == 1) {
+		lame_set_num_channels(lame, 1);
+		lame_set_mode(lame, MONO);
+	}
+	else if (NUM_CHANNELS == 2) {
+		lame_set_num_channels(lame, 2);
+		lame_set_mode(lame, STEREO);
+	}
+	else {
+		std::cout << "Unsupported channel" << std::endl;
+	}
 
 	if (lame_init_params(lame) >= 0) {
 		while (wav.good()) {
@@ -50,7 +63,18 @@ bool LameMp3Encoder::encode_wav(std::string &input)
 			if (read == 0)
 				write = lame_encode_flush(lame, mp3_buffer, MP3_SIZE);
 			else
-				write = lame_encode_buffer_interleaved(lame, pcm_buffer, read, mp3_buffer, MP3_SIZE);
+			{
+				if (NUM_CHANNELS == 1) {
+					write = lame_encode_buffer(lame, pcm_buffer, NULL, read, mp3_buffer, MP3_SIZE);
+				}
+				else if (NUM_CHANNELS == 2) {
+					write = lame_encode_buffer_interleaved(lame, pcm_buffer, read, mp3_buffer, MP3_SIZE);
+				}
+				else {
+					std::cout << "Unsupported channel" << std::endl;
+				}
+			}
+				
 			mp3.write(reinterpret_cast<char*>(mp3_buffer), write);
 		}
 	}
